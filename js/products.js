e6e7df340295db2ea1cd13695a33dd1ed81dc178
urubarlovento <ivansalvoesnal@gmail.com>
2022-09-01 17:39:49 -0300

    Aparente mente pronto para entrega 2

--- js/products.js
+++ js/products.js
@@ -1,9 +1,80 @@
+//ORDENAR SEGUN LOS CRITERIOS
+const ORDER_DESC_BY_COST = "costDesc";
+const ORDER_ASC_BY_COST = "costAsc";
+const ORDER_DESC_BY_SOLDCOUNT = "desSoldCount";
+
+let currentProductsArray = [];
+let currentSortCriteria = undefined;
+let minCount = undefined;
+let maxCount = undefined;
+
+function sortProducts(criteria, array){
+  console.log(array)
+    let result = [];
+    if (criteria === ORDER_DESC_BY_COST)
+    {
+        result = array.sort(function(a, b) {
+          let aCount = parseInt(a.cost);
+          let bCount = parseInt(b.cost);
+
+          if ( aCount > bCount ){ return -1; }
+          if ( aCount < bCount ){ return 1; }
+          return 0;
+      });
+       }
+       
+    
+    else if (criteria === ORDER_ASC_BY_COST){
+      result = array.sort(function(a, b) {
+          let aCount = parseInt(a.cost);
+          let bCount = parseInt(b.cost);
+
+          if ( aCount > bCount ){ return 1; }
+          if ( aCount < bCount ){ return -1; }
+          return 0;
+      });
+  }
+    
+    else if (criteria === ORDER_DESC_BY_SOLDCOUNT)
+    {
+      
+      result = array.sort(function(a, b) {
+        if ( a.soldCount > b.soldCount ){ return -1; }
+        if ( a.soldCount < b.soldCount ){ return 1; }
+        return 0;
+    });
+    }
+
+    return result;
+}
+
+//funcion que manda a ordenar el arreglo
+function sortAndShowProducts(sortCriteria, productsArray){
+  console.log(currentProductsArray)
+
+  currentSortCriteria = sortCriteria;
+
+  if(productsArray != undefined){
+    currentProductsArray = productsArray ;
+  }
+  console.log(productsArray)
+  currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+  console.log(currentProductsArray)
+  //Muestro los productos ordenados
+  showProductsList();
+}
+
+
 
 function showProductsList(){
 
-    let htmlContentToAppend = "";
+ 
+  let htmlContentToAppend = "";
     for(let i = 0; i < currentProductsArray.length; i++){
         let product = currentProductsArray[i];
+
+        if (((minCount == undefined) || (minCount != undefined && parseInt(product.cost) >= minCount)) &&
+        ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))){
 
             htmlContentToAppend += `
             <div onclick="setProdID(${product.id})" class="list-group-item list-group-item-action cursor-active">
@@ -28,44 +99,13 @@ function showProductsList(){
         <div class="text-center p-4">
         <h2>Productos</h2>
         <p> Verás aquí todos los productos de la categoria ${currentCategoryArray.catName}
-        </div>
-        <div class="row">
-        <div class="col text-end">
-          <div class="btn-group btn-group-toggle mb-4" data-bs-toggle="buttons">
-            <input type="radio" class="btn-check" name="options" id="sortAsc">
-            <label class="btn btn-light" for="sortAsc">A-Z</label>
-            <input type="radio" class="btn-check" name="options" id="sortDesc">
-            <label class="btn btn-light" for="sortDesc">Z-A</label>
-            <input type="radio" class="btn-check" name="options" id="sortByCount" checked>
-            <label class="btn btn-light" for="sortByCount"><i class="fas fa-sort-amount-down mr-1"></i></label>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-      <div class="col-lg-6 offset-lg-6 col-md-12 mb-1 container">
-        <div class="row container p-0 m-0">
-          <div class="col">
-            <p class="font-weight-normal text-end my-2">Cant.</p>
-          </div>
-          <div class="col">
-            <input class="form-control" type="number" placeholder="min." id="rangeFilterCountMin">
-          </div>
-          <div class="col">
-            <input class="form-control" type="number" placeholder="máx." id="rangeFilterCountMax">
-          </div>
-          <div class="col-3 p-0">
-            <div class="btn-group" role="group">
-              <button class="btn btn-light btn-block" id="rangeFilterCount">Filtrar</button>
-              <button class="btn btn-link btn-sm" id="clearRangeFilter">Limpiar</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>`;
+        </div>`
+
+
              
-        document.getElementById("products-list-container").innerHTML = htmlContentToAppend2 + htmlContentToAppend;
-    
-}
+        document.getElementById("products-list-container").innerHTML = htmlContentToAppend;
+        document.getElementById("products-sub-title").innerHTML = htmlContentToAppend2;   
+}}
 productNumber= localStorage.getItem("catID");
 let CATE_URL = `https://japceibal.github.io/emercado-api/cats_products/${productNumber}.json`;
 
@@ -79,5 +119,52 @@ document.addEventListener("DOMContentLoaded", function(e){
             showProductsList()
         }
     });
+    //Le da valor a cada uno de los botones de filtro
+    document.getElementById("pSortDesc").addEventListener("click", function(){
+      sortAndShowProducts(ORDER_DESC_BY_COST);
+  });
+    document.getElementById("pSortAsc").addEventListener("click", function(){
+      sortAndShowProducts(ORDER_ASC_BY_COST);
+  });
+    document.getElementById("sortBySoldCount").addEventListener("click", function(){
+     sortAndShowProducts(ORDER_DESC_BY_SOLDCOUNT);
+  });
 
+  //Define la funcion de limpiar y muetra
+  document.getElementById("clearRangeFilter").addEventListener("click", function(){
+    document.getElementById("rangeFilterCountMin").value = "";
+    document.getElementById("rangeFilterCountMax").value = "";
+
+    minCount = undefined;
+    maxCount = undefined;
+
+    showProductsList();
+  })
+
+
+  //Asigna maximo y minimo para filtrar por cantidad de ventas de producto.
+  document.getElementById("rangeFilterCount").addEventListener("click", function(){
+   
+    minCount = document.getElementById("rangeFilterCountMin").value;
+    console.log(minCount)
+    maxCount = document.getElementById("rangeFilterCountMax").value;
+    console.log(maxCount)
+
+    if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+        minCount = parseInt(minCount);
+    }
+    else{
+        minCount = undefined;
+    }
+
+    if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+        maxCount = parseInt(maxCount);
+    }
+    else{
+        maxCount = undefined;
+    }
+
+    showProductsList();
 });
+
+})
