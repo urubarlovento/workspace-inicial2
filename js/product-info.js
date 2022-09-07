3ba2f96427720fa0c6a146e9591bfa256094ce51
urubarlovento <ivansalvoesnal@gmail.com>
2022-09-07 16:31:41 -0300

    falta el desafiate

--- js/product-info.js
+++ js/product-info.js
@@ -25,7 +25,8 @@ document.addEventListener("DOMContentLoaded", function(e){
             // currentProductsArray = currentCategoryArray.products
             showProductComents()
             }})
-    inputcomment()
+
+
 
 });
 
@@ -127,7 +128,7 @@ function showProductInfo(){
         <span style="color:yellow">★★</span><span style="color:grey">★★★</span>`
         if (rate ==3)
         return `
-        <span style="color:yellow">★★★</span><span style="color:grey">★★</span>`
+        <span style="color:yellow">★★★</span><span style="color:grey>★★</span>`
         if (rate == 4)
         return `
         <span style="color:yellow">★★★★</span><span style="color:grey">★</span>`
@@ -164,23 +165,38 @@ function showProductInfo(){
         }
         htmlContentToAppend5= `
         </div>
-        div>
+        </div>
         `
         
             document.getElementsByClassName("container")[1].innerHTML += htmlContentToAppend3+htmlContentToAppend4;
-                 
-        }
+            inputcomment()        
+    }
+
 function inputcomment(){
-let htmlContentToAppend5 = "";
+let htmlContentToAppend5 = document.getElementsByClassName("container")[1].innerHTML
+let htmlContentToAppend6 = "";
 htmlContentToAppend5 =`
 <div class="col">
 <div class="d-flex w-100 justify-content-between">
     <h4 class="mb-1">Comentar</h4>
  </div>
+ <form action="">
+    <p class="mb-1">Tu opinion:</p> 
+     <textarea id="cuerpo" name="cuerpo" rows="3" cols="60"></textarea>
+     <br>
+     <label for="stars">Tu puntuación:</label><br>
+        <select name="stars">
+        <option value = "1">1</option>
+        <option value = "2">2</option>
+        <option value = "3">3</option>
+        <option value = "4">4</option>
+        <option value = "5">5</option>
+        </select><br>
 
+     <input type="submit" class="boton-send" value="Enviar"> </input>        
+</form>
  <div>
+ <br><br>
 `
-document.getElementsByClassName("container")[1].innerHTML += htmlContentToAppend5
+document.getElementsByClassName("container")[1].innerHTML += htmlContentToAppend5+htmlContentToAppend6
 }    
-
-
